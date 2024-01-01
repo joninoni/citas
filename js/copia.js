7e8eeb90244346ae886346c07f689862980322cb
joninoni <jonibeltranhayer@gmail.com>
2023-12-31 18:55:49 -0600

    funcion que guarda los valores de la cita en un objecto

--- js/copia.js
+++ js/copia.js
@@ -1,1 +1,38 @@
-console.log("hola hello");
+//variables
+const formulario = document.querySelector("#nueva-cita");
+const inputMascota = document.querySelector("#mascota");
+const inputPropietario = document.querySelector("#propietario");
+const inputTelefono = document.querySelector("#telefono");
+const inputFecha = document.querySelector("#fecha");
+const inputHora = document.querySelector("#hora");
+const inputSintomas = document.querySelector("#sintomas");
+//objecto Principal
+const objCita={
+    mascota:"",
+    propietario:"",
+    telefono:"",
+    fecha:"",
+    hora:"",
+    sintomas:"",
+}
+// eventos
+listaEventos();
+function listaEventos(){
+    formulario.addEventListener("input",leerDatos)
+    inputMascota.addEventListener("input",leerDatos)
+    inputPropietario.addEventListener("input",leerDatos)
+    inputTelefono.addEventListener("input",leerDatos)
+    inputFecha.addEventListener("input",leerDatos)
+    inputHora.addEventListener("input",leerDatos)
+    inputSintomas.addEventListener("input",leerDatos)
+}
+// classes
+class Cita{
+
+}
+class UI{
+
+}
+function leerDatos(e){
+    objCita[e.target.name]=e.target.value;
+}
