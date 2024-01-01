007fca9f7566d308e795562e026d954748c3ac04
joninoni <jonibeltranhayer@gmail.com>
2023-12-31 22:13:31 -0600

    funcion que valida el formulario y muestra un mensaje

--- js/copia.js
+++ js/copia.js
@@ -6,6 +6,44 @@ const inputTelefono = document.querySelector("#telefono");
 const inputFecha = document.querySelector("#fecha");
 const inputHora = document.querySelector("#hora");
 const inputSintomas = document.querySelector("#sintomas");
+
+const contenido =document.querySelector("#contenido");
+
+// classes
+class Cita{
+    constructor(){
+        this.citas =[];
+    }
+}
+class UI{
+    mostrarMensaje(mensaje,error){
+        const alerta =contenido.querySelector(".alert");
+        if(!alerta){//evita alertas multiples
+            const divMensaje = document.createElement("div");
+            divMensaje.classList.add("alert","text-center","col-12","d-block");
+            if(error){
+                divMensaje.classList.add("alert-danger");
+            }
+            else{
+                divMensaje.classList.add("alert-success");
+            }
+            divMensaje.textContent=mensaje;
+            //insertar en el DOM
+            contenido.insertBefore(divMensaje,document.querySelector(".agregar-cita"));
+
+            setTimeout(()=>{
+                divMensaje.remove();
+            },2000);
+        }
+
+        
+    }
+}
+//intancias de las Class
+const cita =new Cita();
+const ui =new UI();
+
+
 //objecto Principal almacena la informacion de la cita en el objecto
 const objCita={
     mascota:"",
@@ -18,21 +56,24 @@ const objCita={
 // eventos
 listaEventos();
 function listaEventos(){
-    formulario.addEventListener("input",leerDatos)
-    inputMascota.addEventListener("input",leerDatos)
-    inputPropietario.addEventListener("input",leerDatos)
-    inputTelefono.addEventListener("input",leerDatos)
-    inputFecha.addEventListener("input",leerDatos)
-    inputHora.addEventListener("input",leerDatos)
-    inputSintomas.addEventListener("input",leerDatos)
-}
-// classes
-class Cita{
+    formulario.addEventListener("input",leerDatos);
+    inputMascota.addEventListener("input",leerDatos);
+    inputPropietario.addEventListener("input",leerDatos);
+    inputTelefono.addEventListener("input",leerDatos);
+    inputFecha.addEventListener("input",leerDatos);
+    inputHora.addEventListener("input",leerDatos);
+    inputSintomas.addEventListener("input",leerDatos);
 
+    formulario.addEventListener("submit",validarDatos);
 }
-class UI{
 
-}
 function leerDatos(e){
     objCita[e.target.name]=e.target.value;
+}
+function validarDatos(e){
+    e.preventDefault();
+    if(Object.values(objCita).includes("")){
+        ui.mostrarMensaje("No puede haber campos vacios","error");
+        return;
+    }
 }
